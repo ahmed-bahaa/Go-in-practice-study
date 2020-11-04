113533fa2e69e34b904b793710855668c48b8501
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-04 02:58:35 +0200

    use functions

--- developers_guide_course/section3/cards/main.go
+++ developers_guide_course/section3/cards/main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of spades"
-	card := "Five of Diamonds"
+	card := ""
+	card = newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
