79ee144aded2af3aedbebf4b53d323c6182faf6c
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-04 19:46:38 +0200

    Moving some codes and add new deck function

--- developers_guide_course/section3/cards/main.go
+++ developers_guide_course/section3/cards/main.go
@@ -2,14 +2,8 @@ package main
 
 func main() {
 	// var card string = "Ace of spades"
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, newCard2()) // note to work you need to run both of files $ go run main.go newcCard2.go
+	cards := newDeck()
+	// cards = append(cards, newCard2()) // note to work you need to run both of files $ go run main.go newcCard2.go
 
 	cards.print()
-	// fmt.Println(card)
-	// fmt.Println(newCard2())
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
