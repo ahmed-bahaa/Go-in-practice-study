79bfc75f190c60c3e6d149eb6406bb5f2c35c140
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-04 20:24:40 +0200

    adding deal functions and slice ranges, return multiple slices

--- developers_guide_course/section3/cards/main.go
+++ developers_guide_course/section3/cards/main.go
@@ -4,6 +4,7 @@ func main() {
 	// var card string = "Ace of spades"
 	cards := newDeck()
 	// cards = append(cards, newCard2()) // note to work you need to run both of files $ go run main.go newcCard2.go
-
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
 }
