197fed7f556308d650c061b1a37bed12d0689c66
ahmed abdelhamid <ahmed.abdelhamid2@vodafone.com>
2020-11-05 17:44:11 +0200

    adding write to file function

--- developers_guide_course/section3/cards/main.go
+++ developers_guide_course/section3/cards/main.go
@@ -6,8 +6,9 @@ func main() {
 	// var card string = "Ace of spades"
 	cards := newDeck()
 	fmt.Println(cards.toString())
+	cards.writeToFile("deck_myfile.txt")
 	// cards = append(cards, newCard2()) // note to work you need to run both of files $ go run main.go newcCard2.go
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
 }
