765ac9345b416aa58ef1248c47ca0e3fac52190a
ahmed abdelhamid <ahmed.abdelhamid2@vodafone.com>
2020-11-05 17:17:05 +0200

    adding tostring function

--- developers_guide_course/section3/cards/main.go
+++ developers_guide_course/section3/cards/main.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var card string = "Ace of spades"
 	cards := newDeck()
+	fmt.Println(cards.toString())
 	// cards = append(cards, newCard2()) // note to work you need to run both of files $ go run main.go newcCard2.go
 	hand, remainingCards := deal(cards, 5)
 	hand.print()
