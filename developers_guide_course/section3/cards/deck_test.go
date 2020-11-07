cffd31ecee6c9931b6be84f1900cbe98ffb74cc0
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-08 00:13:17 +0200

    adding more test

--- developers_guide_course/section3/cards/deck_test.go
+++ developers_guide_course/section3/cards/deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 40 {
 		t.Errorf("Expected deck length of 40, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected deck to start with Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Ten of Clubs" {
+		t.Errorf("Expected deck to end with Ten of Clubs, but got %v", d[len(d)-1])
+	}
 }
