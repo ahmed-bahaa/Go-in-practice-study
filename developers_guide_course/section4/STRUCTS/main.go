6c69e0aeee06234799fa24da0726514ceda3fe5a
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-08 13:16:11 +0200

    embedded struct

--- developers_guide_course/section4/STRUCTS/main.go
+++ developers_guide_course/section4/STRUCTS/main.go
@@ -2,26 +2,44 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// not a good way in declaration
-	//alex := person{"ahmed", "bahaa"}
-
-	alex := person{firstName: "ahmed", lastName: "Bahaa"}
-	fmt.Println(alex)
-
-	// third way in declaration
-	var person2 person
-	fmt.Println(person2)
-	fmt.Printf("%+v", person2)
-
-	fmt.Println("")
-
-	person2.firstName = "blaf"
-	person2.lastName = "blalast"
-	fmt.Printf("%+v", person2)
+	alex := person{
+		firstName: "alex",
+		lastName:  "jim",
+		contact: contactInfo{
+			email:   "alex_jim@gmail.com",
+			zipCode: 555,
+		},
+	}
+	fmt.Println("%+v", alex)
 }
+
+// func main() {
+// 	// not a good way in declaration
+// 	//alex := person{"ahmed", "bahaa"}
+
+// 	alex := person{firstName: "ahmed", lastName: "Bahaa"}
+// 	fmt.Println(alex)
+
+// 	// third way in declaration
+// 	var person2 person
+// 	fmt.Println(person2)
+// 	fmt.Printf("%+v", person2)
+
+// 	fmt.Println("")
+
+// 	person2.firstName = "blaf"
+// 	person2.lastName = "blalast"
+// 	fmt.Printf("%+v", person2)
+// }
