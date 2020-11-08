9cb3fe4757b47bdd8b0bb30301740b467fb3d06c
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-08 12:31:58 +0200

    decalre empty map, add new element and delete element

--- developers_guide_course/section5/MAP/main.go
+++ developers_guide_course/section5/MAP/main.go
@@ -9,5 +9,18 @@ func main() {
 		"blue": "#fff6564564",
 	}
 
+	//declare an empty map 1
+	// var colors2 map[string]string
+
+	//decalre an empty map2
+	color2 := make(map[string]string)
+	fmt.Println("Printing empty colors2: ", color2)
+
+	color2["test-color"] = "#fd5555"
+	fmt.Println("Printing non empty colors2: ", color2)
+
+	delete(color2, "test-color")
+	fmt.Println("Printing empty colors2 after delete: ", color2)
+
 	fmt.Println(colors)
 }
