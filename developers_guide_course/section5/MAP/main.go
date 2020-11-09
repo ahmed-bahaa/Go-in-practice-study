79f152933f6df944c087d24e0caa3cabfb54d433
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-09 03:07:28 +0200

    iterate over map

--- developers_guide_course/section5/MAP/main.go
+++ developers_guide_course/section5/MAP/main.go
@@ -5,22 +5,30 @@ import "fmt"
 func main() {
 
 	colors := map[string]string{
-		"red":  "#fff222",
-		"blue": "#fff6564564",
+		"red":   "#fff222",
+		"blue":  "#fff6564564",
+		"white": "$fffff",
 	}
 
 	//declare an empty map 1
 	// var colors2 map[string]string
 
 	//decalre an empty map2
-	color2 := make(map[string]string)
-	fmt.Println("Printing empty colors2: ", color2)
+	// color2 := make(map[string]string)
+	// fmt.Println("Printing empty colors2: ", color2)
 
-	color2["test-color"] = "#fd5555"
-	fmt.Println("Printing non empty colors2: ", color2)
+	// color2["test-color"] = "#fd5555"
+	// fmt.Println("Printing non empty colors2: ", color2)
 
-	delete(color2, "test-color")
-	fmt.Println("Printing empty colors2 after delete: ", color2)
+	// delete(color2, "test-color")
+	// fmt.Println("Printing empty colors2 after delete: ", color2)
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, " is ", hex)
+	}
 }
