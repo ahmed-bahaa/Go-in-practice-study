e998ad28f420fd3d66899abb8b988c687e8c3694
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-09 15:14:21 +0200

    apply interfaces

--- developers_guide_course/section6/INTERFACES/main.go
+++ developers_guide_course/section6/INTERFACES/main.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreating() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
 
 	eb := englishBot{}
+	sb := spanishBot{}
+
 	printGreeting(eb)
+	printGreeting(sb)
+
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreating())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreating())
 }
 
 func (englishBot) getGreating() string {
