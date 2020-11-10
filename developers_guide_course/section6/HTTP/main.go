c625fc8dcc00a57a6f8d4925885347055affa0f3
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-10 19:29:12 +0200

    custom write func

--- developers_guide_course/section6/HTTP/main.go
+++ developers_guide_course/section6/HTTP/main.go
@@ -7,6 +7,9 @@ import (
 	"os"
 )
 
+type logWriter struct {
+}
+
 func main() {
 
 	resp, err := http.Get("http://google.com")
@@ -21,5 +24,13 @@ func main() {
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
