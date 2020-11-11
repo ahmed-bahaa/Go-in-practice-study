74d48875a6aecf0dcfefd7455ca26f4e186c008b
Ahmed Bahaa <a.bahaarefaii@gmail.com>
2020-11-11 18:50:53 +0200

    rEPEATING ROUTINES

--- developers_guide_course/section7/Channels/main.go
+++ developers_guide_course/section7/Channels/main.go
@@ -21,8 +21,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -30,10 +30,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "Might be down!")
-		c <- "Might be down!"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is Up!")
-	c <- "is Up!!"
+	c <- link
 }
